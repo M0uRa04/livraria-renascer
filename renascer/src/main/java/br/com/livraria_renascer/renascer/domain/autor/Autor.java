9d7d9fdd9af827d94f39eba58a71b3eb63da9fc8
Robson Moura <robsonmoura970@gmail.com>
2024-06-18 11:29:10 -0300

    Criando a Entidade Autor

--- renascer/src/main/java/br/com/livraria_renascer/renascer/domain/autor/Autor.java
+++ renascer/src/main/java/br/com/livraria_renascer/renascer/domain/autor/Autor.java
@@ -1,2 +1,23 @@
-package br.com.livraria_renascer.renascer.domain.autor;public class Autor {
+package br.com.livraria_renascer.renascer.domain.autor;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Table(name = "autor")
+@Data
+@EqualsAndHashCode (of = "id")
+public class Autor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String nome;
+
+    @NotBlank
+    private String biografia;
 }
