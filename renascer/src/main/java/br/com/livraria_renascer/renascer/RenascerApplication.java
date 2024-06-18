b8af1c98cde56583bc77df27f61465847265e96e
Robson Moura <robsonmoura970@gmail.com>
2024-06-18 10:34:02 -0300

    First Commit

---
+++ renascer/src/main/java/br/com/livraria_renascer/renascer/RenascerApplication.java
@@ -0,0 +1,13 @@
+package br.com.livraria_renascer.renascer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenascerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenascerApplication.class, args);
+	}
+
+}
