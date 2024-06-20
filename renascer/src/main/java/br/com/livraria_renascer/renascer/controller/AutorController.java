eb0c279f66e818be6063c8dd834de28d30603374
Robson Moura <robsonmoura970@gmail.com>
2024-06-20 10:26:11 -0300

    Feat: Implementando o repository e persistindo o primeiro registro de autor no banco de dados

--- renascer/src/main/java/br/com/livraria_renascer/renascer/controller/AutorController.java
+++ renascer/src/main/java/br/com/livraria_renascer/renascer/controller/AutorController.java
@@ -1,16 +1,20 @@
 package br.com.livraria_renascer.renascer.controller;
 
+import br.com.livraria_renascer.renascer.domain.autor.Autor;
+import br.com.livraria_renascer.renascer.domain.autor.AutorRepository;
 import br.com.livraria_renascer.renascer.domain.autor.DadosCadastroAutor;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
-
 @RestController
 @RequestMapping("/autor")
 public class AutorController {
+
+    @Autowired
+    private AutorRepository repository;
 
     @GetMapping
     public ResponseEntity listaAutores () {
@@ -20,6 +24,7 @@ public class AutorController {
     @PostMapping
     public ResponseEntity cadastraAutor (@Valid @RequestBody DadosCadastroAutor dados) {
         var uri = ServletUriComponentsBuilder.fromCurrentContextPath().build().toUri();
+        repository.save(new Autor(dados));
         return ResponseEntity.created(uri).body(dados);
     }
 
---
+++ renascer/src/main/resources/db/migration/V2__alter-table-autor-modify-column-nome-unique.sql
@@ -0,0 +1,1 @@
+ALTER TABLE autor ADD CONSTRAINT nome_unique UNIQUE (nome);
