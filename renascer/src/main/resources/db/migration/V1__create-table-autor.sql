00c34d8a5a3f220005f58fe83d9abd6260ca873e
Robson Moura <robsonmoura970@gmail.com>
2024-06-28 09:07:10 -0300

    Feat: Aplicando a lógica de negócios para que os nomes de autores e categorias sejam unicos.

--- renascer/src/main/resources/db/migration/V1__create-table-autor.sql
+++ renascer/src/main/resources/db/migration/V1__create-table-autor.sql
@@ -2,7 +2,7 @@ CREATE TABLE autor (
 
     id bigint unique not null auto_increment,
 
-    nome varchar (100) not null,
+    nome varchar (100) not null unique,
 
     biografia varchar (1000) not null,
 
--- renascer/src/main/resources/db/migration/V4__create-table-categoria.sql
+++ renascer/src/main/resources/db/migration/V4__create-table-categoria.sql
@@ -2,7 +2,7 @@ CREATE TABLE categoria (
 
     id bigint unique not null auto_increment,
 
-    nome varchar (30) not null,
+    nome varchar (30) not null unique,
 
     descricao varchar (1000) not null,
 
