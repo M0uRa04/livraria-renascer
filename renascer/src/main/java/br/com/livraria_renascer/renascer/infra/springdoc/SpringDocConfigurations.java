5448ec8ce8203e01054752e49c7e5538e499ef0c
Robson Moura <robsonmoura970@gmail.com>
2024-07-01 15:23:45 -0300

    Feat: Terminando a implementação da classe de configuração da documentação sobre o projeto.

--- renascer/src/main/java/br/com/livraria_renascer/renascer/infra/springdoc/SpringDocConfigurations.java
+++ renascer/src/main/java/br/com/livraria_renascer/renascer/infra/springdoc/SpringDocConfigurations.java
@@ -2,6 +2,8 @@ package br.com.livraria_renascer.renascer.infra.springdoc;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,7 +16,15 @@ public class SpringDocConfigurations {
         return new OpenAPI()
                 .components(new Components()
                         .addSecuritySchemes("bearer-key",
-                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+                .info(
+                        new Info()
+                                .title("Livraria Renascer API")
+                                .description("API REST da aplicação feita para a livraria renascer.")
+                                .contact(new Contact()
+                                        .name("Robson Moura - Desenvolvedor Back-End")
+                                        .email("robsonmoura970@hotmail.com"))
+                );
     }
 
 
